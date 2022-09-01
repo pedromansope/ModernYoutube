4ac183886aa64de77520f560b3d9a9834f8fc3f6
Pedro Manso <pedrolucasmppe@gmail.com>
2022-09-01 13:32:19 -0300

    Implementa NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 const App = () => {
   return (
