49d4fd3ea780c50a4a1f14f06b240545b24debb7
Pedro Manso <pedrolucasmppe@gmail.com>
2022-09-27 10:37:35 -0300

    visualizao de videos e feed lateral

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,11 +3,11 @@ import { Stack, Box } from '@mui/material';
 import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
-const Videos = ({ videos }) => {
-  console.log(videos);
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return 'Carregando...';
 
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={ direction || "row" } flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
